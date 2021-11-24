7bfd6db66a1d7f214ac073e928650ea9f78ffd4c
aleraiz <alejandroraiz7@gmail.com>
2021-11-23 23:14:49 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+
+class User {
+    
+    constructor (nombre, password){
+        this.nombre = nombre;
+        this.password = password
+    }
+
+}
+
+const alejandro = new User("ale","hola")
+
+
+
+const confirmacionDeAcceso = () => {
+
+    let bucle = true
+
+    while (bucle) {
+        const nombre = prompt("Ingresa tu nombre");
+        const password = prompt("Ingresa tu pass");
+
+        if(nombre === alejandro.nombre && password === alejandro.password){
+
+            bucle = false
+            break;
+
+        } else {
+            alert("Usuario incorrecto");
+        }
+
+    }
+}
+
+confirmacionDeAcceso()
--- js/productos.js
+++ js/productos.js
@@ -1,55 +1,212 @@
+// ENTIDADES
+
+class AñadirProductoCarrito{
+
+    constructor ({name,category,price,image,id}){
+
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.image = image;
+        this.id = id;
+    }  
+}
+
+
+//CLASES
+//VARIABLES
+//FUNCIONES
+//LOGICA
+
+////////////////////////      VARIABLES      ///////////////////////
+
+let arrayProductos=[];
+let lista=[];
+let listaTodosLosProductos;
+let listaProductosCarrito=[];
+let imprimirTodosLosProductos = $(".contenidoProductosTarjetas");
+let imprimirProductosCarrito = $("#carrito-compras-imprimir");
 
 
 
 
-
+////////////////         LLAMADA AJAX - API PRODUCTOS      ////////////////////
 
 const url = `https://618ac9e034b4f400177c48a4.mockapi.io/api/v1/products`
 
     $.get(url, (respuesta, estado) =>{
 
-        if(estado == "success"){
+        if(estado == "success") {
+            // console.log(respuesta);
+            
+            respuesta.forEach(element => {
+                // console.log(element.id);
 
-
-            for(let i =0; i< respuesta.length; i++){
-
-                $(".contenidoProductosTarjetas").append(
-                    `
-                    <div class="producto-container">
-                        <img src="${respuesta[i].image}">
-                        <h3 class="texto-productos">${respuesta[i].name}</h3>
-                        <h4 class="texto-productos">${respuesta[i].price}</h4>
-                        <span><button>Agregar</button></span>
-                    </div>
-                    `)
-            }
+            });
+            arrayProductos.push(respuesta)
+        localStorage.setItem("listaTodosLosProductos",JSON.stringify(arrayProductos[0]));
+            // console.log(arrayProductos[0]);
         }
     })
+    
+    
+
+if(localStorage.getItem("listaTodosLosProductos") == null){
+    console.log("No hay productos para mostrar");
+} else {
+    listaTodosLosProductos = JSON.parse(localStorage.getItem("listaTodosLosProductos"))
+}
+
+// console.log(listaTodosLosProductos);
+
+listaTodosLosProductos.forEach(element => {
+
+    let index = listaTodosLosProductos.indexOf(element)
+    // console.log(index);
+
+    imprimirTodosLosProductos.append(
+        `
+        <div class="producto-container">
+            <img src="${element.image}">
+            <h3 class="texto-productos">${element.name}</h3>
+            <h4 class="texto-productos">$ ${element.price}</h4>
+            <span><button class="btn-comprar" onclick="comprar(${index})">Agregar <i class="usuario__iconos fas fa-shopping-bag" aria-hidden="true"></i></button></span>
+        </div>
+        `
+    );
+})
 
 
 
+// Comprar productos - Agregar productos al carrito
+
+const comprar = (index) =>{
+    if (listaProductosCarrito.lenght == null) {
+        listaProductosCarrito = [];
+    } else {
+        listaProductosCarrito = JSON.parse(localStorage.getItem("listaCarrito"))
+    }
+    console.log(listaProductosCarrito);
+    listaProductosCarrito.push(listaTodosLosProductos[index])
+    localStorage.setItem("listaCarrito",JSON.stringify(listaProductosCarrito))
+}
+
+// Imprimir productos del carrito(localstorage) en carrito
+
+if(localStorage.getItem("listaCarrito") == null){
+    console.log("No hay productos para mostrar");
+} else {
+    listaProductosCarrito = JSON.parse(localStorage.getItem("listaCarrito"))
+}
+
+// console.log(listaProductosCarrito);
+
+if (listaProductosCarrito.length != 0) {
+    listaProductosCarrito.forEach(element =>{
+
+        imprimirProductosCarrito.append(
+            `
+            <div class="producto-carrito">
+                <div>
+                    <img src="${element.image}">
+                </div>
+                <div class="producto-carrito-info">
+                    <p>${element.name}</p>
+                    <p>$ ${element.price}</p>
+                    <i class="fas fa-times" id="btn-eliminar-producto-carrito"></i>
+                </div>
+            </div>
+            
+            `
+        )
+    })
+} else {
+    imprimirProductosCarrito.append(
+        `
+        <div class="producto-carrito">
+            <div class="producto-carrito-info">
+                <p>Carrito vacío</p>
+            </div>
+        </div>
+        
+        `
+    )
+}
+
+// Compra en carrito
+
+const finalizarCompra = ()=>{
+
+    let monto = 0
+
+    listaProductosCarrito.forEach(element => {
+        monto += JSON.parse(element.price)
+    });
+
+    document.getElementById("total-carrito-compra").textContent = `$ ${monto}`;
+}
+
+finalizarCompra()
 
 
 
+////////////////////////////    DARK MODE     /////////////////////////
 
 
-// $(`#btn`).click( ()=>{
+const darkMode = () => {
+    document.getElementById("linkTheme").href = "../estilos/darkMode/darkMode.css"
+    localStorage.setItem("theme", "dark")
+}
 
-//     $.get(url, (respuesta, estado) =>{
+const lightMode = () => {
+    document.getElementById("linkTheme").href = "../estilos/main.css"
+    localStorage.setItem("theme", "light")
+}
 
-//         if(estado == "success"){
+document.getElementById("btn-darkMode").addEventListener("click", ()=>{
+    if(localStorage.getItem("theme") === "dark"){
+        lightMode()
+    } else {
+        darkMode()
+    }
+})
 
-//             console.log(respuesta[0])
-//             $(".container-div").prepend(
-//                 `
-//                 <img src="${respuesta[0].image}">
-//                 <p>${respuesta[0].name}</p>
-//                 <p>${respuesta[0].price}</p>
-//                 `)
-//         }
+////////////////////////////   SHOW/HIDE CARRITO DE COMPRAS     /////////////////////////
 
+const carritoShow = () =>{
+    $(".carrito-compras").css("top", "60px");
+    localStorage.setItem("carrito","show")
+}
 
-//     })
+const carritoHide = () =>{
+    $(".carrito-compras").css("top", "-900px");
+    localStorage.setItem("carrito","hide")
+}
 
+$("#btn-carrito-compras").on("click", ()=>{
+    if(localStorage.getItem("carrito") === "show"){
+        carritoHide()
+    } else {
+        carritoShow()
+    }
+});
 
-// })
+//////////////////////////   SHOW/HIDE ingresar-cuenta     /////////////////////////
+
+const cuentaShow = () =>{
+    $(".cuenta-usuario").css("top", "60px");
+    localStorage.setItem("cuenta","show")
+}
+
+const cuentaHide = () =>{
+    $(".cuenta-usuario").css("top", "-900px");
+    localStorage.setItem("cuenta","hide")
+}
+
+$("#btn-cuenta-usuario").on("click", ()=>{
+    if(localStorage.getItem("cuenta") === "show"){
+        cuentaHide()
+    } else {
+        cuentaShow()
+    }
+});
